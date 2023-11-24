a823c736108471f535ff716b5ea4d402c930767c
Karl Mårtensson <karlmartenssonsales@gmail.com>
2023-11-24 17:00:56 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Funktion_lektion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
